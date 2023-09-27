1fe751b5cd1b84f56bb7b51cf06720689384dea3
James Hallberg <Grebllah81788@gmail.com>
2023-09-27 13:57:10 -0400

    complete react events and state lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setSelectedCategory(e.target.value)
+  }
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +19,15 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {
+          selectedCategory !== "All" ?
+            items
+              .filter((item) => {return item.category === selectedCategory})
+              .map((item) => (<Item key={item.id} name={item.name} category={item.category} />))
+            :
+            items
+              .map((item) => (<Item key={item.id} name={item.name} category={item.category} />))
+        }
       </ul>
     </div>
   );
